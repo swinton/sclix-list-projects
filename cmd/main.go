409d6224e0527b0bc1cb1741da6cdc58f3398f72
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2022-08-15 12:23:11 +0200

    chore: add additional and improve existing error handling

--- cmd/main.go
+++ cmd/main.go
@@ -10,15 +10,21 @@ import (
 	cli_extension_lib_go "github.com/snyk/cli-extension-lib-go"
 )
 
-func main() {
-	// Bootstrap Extension
-	_, extensionInput, err := cli_extension_lib_go.InitExtension()
+type ProjectsAPIResponse struct {
+	Data []struct {
+		ID         string `json:"id"`
+		Attributes struct {
+			Name string `json:"name"`
+		}
+	} `json:"data"`
+}
+
+func mainE(extensionInput *cli_extension_lib_go.ExtensionInput) (err error) {
+	t, err := extensionInput.GetHttpTransport()
 	if err != nil {
-		fmt.Println("Error initializing extension")
-		fmt.Println(err)
-		os.Exit(1)
+		return fmt.Errorf("Failed to get Transport object. %v", err)
 	}
-	t, _ := extensionInput.GetHttpTransport()
+
 	httpClient := &http.Client{
 		Transport: t,
 	}
@@ -26,9 +32,7 @@ func main() {
 	// Check if we have token available for API Requests
 	apiToken := extensionInput.Token
 	if len(apiToken) == 0 {
-		fmt.Println("Missing Snyk API token. Run snyk auth first.")
-		fmt.Println(err)
-		os.Exit(1)
+		return fmt.Errorf("Missing Snyk API token. Run snyk auth first.")
 	}
 	apiAuthHeader := "Token " + apiToken
 
@@ -36,9 +40,7 @@ func main() {
 	var orgId string
 	orgId, err = extensionInput.Command.StringOptionValue("org")
 	if err != nil {
-		fmt.Println("Invalid input")
-		fmt.Println(err)
-		os.Exit(1)
+		return fmt.Errorf("Invalid input %v", err)
 	}
 
 	if len(orgId) == 0 {
@@ -49,53 +51,51 @@ func main() {
 			} `json:"data"`
 		}
 
-		req, _ := http.NewRequest("GET", "https://api.snyk.io/rest/orgs/?version=2022-04-06~experimental", nil)
+		req, err := http.NewRequest("GET", "https://api.snyk.io/rest/orgs/?version=2022-04-06~experimental", nil)
+		if err != nil {
+			return err
+		}
+
 		// Headers
 		req.Header.Add("Authorization", apiAuthHeader)
 		req.Header.Add("Content-Type", "application/vnd.api+json")
 		resp, err := httpClient.Do(req)
 		if err != nil {
-			fmt.Println("Request failure : ", err)
+			return fmt.Errorf("Request failure: %v", err)
 		}
 		respBody, _ := ioutil.ReadAll(resp.Body)
 		var result OrgsAPIResponse
 		if err := json.Unmarshal(respBody, &result); err != nil {
-			fmt.Println("Can not unmarshal JSON")
+			return fmt.Errorf("Can not unmarshal JSON. %v", err)
 		}
 
 		if len(result.Data) == 0 {
-			fmt.Println("Invalid input")
-			fmt.Println(err)
-			os.Exit(1)
+			return err
 		}
 		orgId = result.Data[0].ID
 	}
 
-	type ProjectsAPIResponse struct {
-		Data []struct {
-			ID         string `json:"id"`
-			Attributes struct {
-				Name string `json:"name"`
-			}
-		} `json:"data"`
-	}
 	fmt.Println("Checking projects in Org with ID: " + orgId + "\n")
-	req, _ := http.NewRequest("GET", "https://api.snyk.io/rest/orgs/"+orgId+"/projects?version=2022-04-06~experimental", nil)
+	req, err := http.NewRequest("GET", "https://api.snyk.io/rest/orgs/"+orgId+"/projects?version=2022-04-06~experimental", nil)
+	if err != nil {
+		return err
+	}
+
 	req.Header.Add("Authorization", apiAuthHeader)
 	req.Header.Add("Content-Type", "application/vnd.api+json")
 	resp, err := httpClient.Do(req)
 	if err != nil {
-		fmt.Println("Request failure : ", err)
+		return fmt.Errorf("Request failure : %v", err)
 	}
+
 	respBody, _ := ioutil.ReadAll(resp.Body)
 	var result ProjectsAPIResponse
 	if err := json.Unmarshal(respBody, &result); err != nil {
-		fmt.Println("Can not unmarshal JSON")
+		return fmt.Errorf("Can not unmarshal JSON. %v", err)
 	}
 
 	if len(result.Data) == 0 {
-		fmt.Println("No projects to list")
-		os.Exit(0)
+		return fmt.Errorf("No projects to list")
 	}
 
 	fmt.Printf("Found projects: %d\n\n", len(result.Data))
@@ -104,4 +104,33 @@ func main() {
 		fmt.Println("- " + project.Attributes.Name)
 	}
 	fmt.Println(result.Data[0].Attributes.Name)
+
+	return nil
+}
+
+func main() {
+	exitCode := 0
+
+	// Bootstrap Extension
+	_, extensionInput, err := cli_extension_lib_go.InitExtension()
+	if err != nil {
+		fmt.Println("[list extensions] Error:", err)
+		os.Exit(1)
+	}
+
+	if extensionInput.Debug {
+		fmt.Println("[list extensions] Starting")
+	}
+
+	err = mainE(extensionInput)
+	if err != nil {
+		fmt.Println("[list extensions] Error:", err)
+		exitCode = 1
+	}
+
+	if extensionInput.Debug {
+		fmt.Printf("[list extensions] Exiting with %d \n", exitCode)
+	}
+
+	os.Exit(exitCode)
 }
